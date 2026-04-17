e24cc4d28acfad2237696aaceb33c7e131b8601e
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2026-04-17 18:15:10 +0600

    added home screen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,11 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <SafeAreaView>
+      <ScrollView>
+        <Text>Home Screen</Text>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
