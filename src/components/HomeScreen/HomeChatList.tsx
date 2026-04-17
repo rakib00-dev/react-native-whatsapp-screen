c7900988eb141337c1b8fa65b0cade407487d7e6
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2026-04-17 18:19:37 +0600

    added three components

---
+++ src/components/HomeScreen/HomeChatList.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function HomeChatList() {
+  return (
+    <View>
+      <Text>HomeChatList</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
---
+++ src/components/HomeScreen/HomeHeader.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function HomeHeader() {
+  return (
+    <View>
+      <Text>HomeHeader</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
