2ece3842c0a73544998adbeee23344b55fb99ce9
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2026-04-18 13:18:08 +0600

    add components

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,19 @@
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import HomeHeader from '../components/HomeScreen/HomeHeader';
+import HomeChatFilter from '../components/HomeScreen/HomeChatFilter';
+import HomeChatList from '../components/HomeScreen/HomeChatList';
 
 export default function HomeScreen() {
-  return (
+
+
+  return (  
     <SafeAreaView>
       <ScrollView>
-        <Text>Home Screen</Text>
+        <HomeHeader/>
+        <HomeChatFilter/>
+        <HomeChatList/>
       </ScrollView>
     </SafeAreaView>
   );
