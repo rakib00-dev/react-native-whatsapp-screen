c3ff76b9ef21770f2bf11bdfbfe6eb4f4c474bb8
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2026-04-18 19:22:37 +0600

    added home screen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import React, { JSX } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import HomeScreen from './src/screens/HomeScreen';
 
 export default function App(): JSX.Element {
   return (
     <SafeAreaView>
-      <ScrollView>App</ScrollView>
+      <ScrollView>
+        <HomeScreen />
+      </ScrollView>
     </SafeAreaView>
   );
 }
